bb8b33b3fab0789c5baf6f0679be54afdbf1356d
Kresna Sucandra <kresnasucandra@gmail.com>
2021-06-27 20:51:54 +0800

    Add std::io

---
+++ learn_rust/target/debug/deps/learn_rust-6ed418b0c77c73f0.d
@@ -0,0 +1,5 @@
+/home/sha888/projects/rustByExample/learn_rust/target/debug/deps/learn_rust-6ed418b0c77c73f0.rmeta: src/main.rs
+
+/home/sha888/projects/rustByExample/learn_rust/target/debug/deps/learn_rust-6ed418b0c77c73f0.d: src/main.rs
+
+src/main.rs:
