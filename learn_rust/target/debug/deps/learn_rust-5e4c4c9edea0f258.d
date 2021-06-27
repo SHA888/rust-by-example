980afa46123acda57288952919757932f1a77c11
Kresna Sucandra <kresnasucandra@gmail.com>
2021-06-27 20:42:44 +0800

    Started learning path

---
+++ learn_rust/target/debug/deps/learn_rust-5e4c4c9edea0f258.d
@@ -0,0 +1,5 @@
+/home/sha888/projects/rustByExample/learn_rust/target/debug/deps/learn_rust-5e4c4c9edea0f258: src/main.rs
+
+/home/sha888/projects/rustByExample/learn_rust/target/debug/deps/learn_rust-5e4c4c9edea0f258.d: src/main.rs
+
+src/main.rs:
---
+++ learn_rust/target/debug/learn_rust.d
@@ -0,0 +1,1 @@
+/home/sha888/projects/rustByExample/learn_rust/target/debug/learn_rust: /home/sha888/projects/rustByExample/learn_rust/src/main.rs
