49337e3fa0ad4d00740597e3834b28d289235d46
Kresna Sucandra <kresnasucandra@gmail.com>
2021-07-03 13:45:29 +0800

    Added variable exercise

---
+++ variables/target/debug/deps/variables-3b736149aabe80bf.d
@@ -0,0 +1,5 @@
+/home/sha888/projects/rustByExample/variables/target/debug/deps/variables-3b736149aabe80bf: src/main.rs
+
+/home/sha888/projects/rustByExample/variables/target/debug/deps/variables-3b736149aabe80bf.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/sha888/projects/rustByExample/variables/target/debug/variables: /home/sha888/projects/rustByExample/variables/src/main.rs
