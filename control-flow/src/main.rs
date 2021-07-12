37d793e6146ba3f40c55bc976a8143827889c3fe
Kresna Sucandra <kresnasucandra@gmail.com>
2021-07-12 16:13:49 +0800

    Add control flow

---
+++ control-flow/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let number2 = 7;
+
+    if number2 < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+    
+    let number3 = 3;
+
+    if number3 != 0 {
+        println!("condition was something other than zero");
+    }
+
+    // Handling Multiple Conditions with else if
+}
