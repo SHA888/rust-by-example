86d7125a7e183cf2c199105489842115ab35b806
Kresna Sucandra <kresnasucandra@gmail.com>
2021-07-15 11:21:31 +0800

    Update control-flow

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -22,4 +22,15 @@ fn main() {
     }
 
     // Handling Multiple Conditions with else if
+    let number4 = 6;
+
+    if number4 % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number4 % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number4 % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
