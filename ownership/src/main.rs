491a54cbfc51c504d5cf4db25f3da7de91d6b4c3
Kresna Sucandra <kresnasucandra@gmail.com>
2021-07-26 16:06:05 +0800

    Add ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+
+    // The Stack and the Heap -> part of memory management -> to be used at runtime
+
+    // The Stack 
+    // `last in, first out`
+    // `pushing onto the stack` and `popping off the stack`
+    // must have a known, fixed size
+    // unknown data size at compile time or a size that might change -> stored at the Heap (less organized` )
+    // 
+    // The Heap
+    // memory allocator finds an empty spot in the heap that big enough, marks it as being in use, 
+    // and return a `pointer` (address of that location) -> `allocating on the heap`
+    // 
+    // Ownership Rules:
+
+
+    println!("Ownership");
+}
