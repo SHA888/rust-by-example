a2d4522fbcab45e4923ef552e3a2c54150c34c2d
Kresna Sucandra <kresnasucandra@gmail.com>
2021-08-14 21:46:49 +0800

    Copy

--- src/ownership.rs
+++ src/ownership.rs
@@ -20,10 +20,10 @@ pub fn main() {
     // Variable Scope
     // ----------------------------------------------------------------
 
-    let s1 = "hello";
+    let _s1 = "hello";
 
     {
-        let s2 = "world";
+        let _s2 = "world";
         // do stuff with s2
     }
 
@@ -39,4 +39,19 @@ pub fn main() {
     // - The memory must be requested from the memory allocator at runtime.
     // - We need a way of returning this memory to the allocator when we're done with our `String`.
 
+    // Shallow copy and deep copy
+    // Move and Clone
+
+    let s4 = String::from("Hello");
+    let s5 = s4.clone();
+
+    println!("s4 = {}, s5 = {}", s4, s5);
+
+    // Some of the type that implement `Copy`
+    // - All the integer types, such as `u32`
+    // - The boolean type, `bool`, with values `true` and `false`
+    // - All the floating point types, such as `f64`
+    // - The character type, `char`
+    // - Tuples, if they only contain types that also implement `Copy`. For example, `(i32, i32)`
+    //   implements `Copy`, but `(i32, String)` does not
 }
